b3303adc7a72c898ac30191b4b5980c162439c09
anthony.ras <ayolachll@gmail.com>
2016-02-09 22:27:56 +0800

    try to make the big ship to be the target on drag event

--- js/Battleship.js
+++ js/Battleship.js
@@ -1,27 +1,30 @@
 var Battleship = {};
 Battleship.enemyBoard = {};
 Battleship.myBoard = {};
-Battleship.MainMenu = function(game) {}
+Battleship.Game = function(game) {}
 
-Battleship.MainMenu.prototype = {
+Battleship.Game.prototype = {
     preload: function(game) {
         game.load.image('ship', 'assets/battleship.png');
         game.stage.backgroundColor = "#0B65F8";
     },
     render: function(game) {
         Battleship.Ships.forEach(function(ship) {
-            game.debug.body(ship);
+            // game.debug.body(ship);
         });
     },
     create: function(game) {
-        // console.log(Battleship.myBoard.x, Battleship.myBoard.y)
         Battleship.Ships = game.add.group();
+        Battleship.EnemyShip = game.add.group();
+        Battleship.EnemyShip.enableBody = true;
         Battleship.Ships.enableBody = true;
+
+        // create sample battleshipEnemy
+        Battleship.EnemyShip.create(0,0, 'ship');
+        Battleship.EnemyShip.tint = 0xff00ff;
         var maxRow = 3;
         var maxCol = 5;
-        var shipWidth = 90;
-        var shipHeight = 110;
-        var padding = 80
+        var padding = 70
         var X = 0;
         var Y = 0;
         var posY = 0;
@@ -31,7 +34,7 @@ Battleship.MainMenu.prototype = {
             posX = 0;
             for(var col = 0; maxCol > col; col++) {
                 var shipModel = new Phaser.Sprite(game, posX, posY,'ship');
-                shipModel.scale.setTo(0.1,0.1);
+                shipModel.scale.setTo(0.3,0.3);
                 shipModel.enableBody = true;
                 shipModel.inputEnabled = true;
                 game.add.tween(shipModel).to({ y: posY - Math.floor((Math.random() * 10) + 1), x: posX - Math.floor((Math.random() * 10) + 1) }, 1100, "Linear", true, 1, 20, true).loop(true);
@@ -41,17 +44,16 @@ Battleship.MainMenu.prototype = {
 
                 Battleship.Ships.addChild(shipModel);
 
-                posX += shipWidth;
+                posX += padding;
 
-
-                 // var ship = game.add.sprite(posX, posY, 'ship');
-                 // ship.scale.setTo(0.5, 0.5);
-                 // game.add.tween(ship).to({ y: posY-10 }, 1100, "Linear", true, 1, 20, true).loop(true);
-                 
-                 // add eventListener to ship
             }
-            posY += shipHeight;
+            posY += padding;
         }  // end loop
+
+        // loop all battleships and bind events here.
+        // Battleship.Ships.forEachAlive(function(ship) {
+        //     ship.kill();
+        // })
         Battleship.Ships.x = game.world.centerX - (Battleship.Ships.width/2);
         Battleship.Ships.y = game.world.centerY - (Battleship.Ships.height/2);
         game.physics.arcade.enable(Battleship.Ships);
--- js/game.js
+++ js/game.js
@@ -2,6 +2,6 @@
     var width = window.innerWidth;
     var height = window.innerHeight;
     var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game');
-    game.state.add('menu', BShip.MainMenu );
-    game.state.start('menu');
+    game.state.add('game', BShip.Game );
+    game.state.start('game');
 })(Phaser, Battleship, window);
