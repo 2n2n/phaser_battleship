feb284fa2ed6eac8e0d0036e91522b25a3a1d87a
2n2n <ayolachll@gmail.com>
2016-02-12 17:40:42 +0800

    battleship single trying to locate the point of location.

--- js/BattleshipSingle.js
+++ js/BattleshipSingle.js
@@ -7,14 +7,17 @@ BattleshipSingle.Game.prototype = {
         game.load.image('cloud', 'assets/cloud_1.png');
     },
     create: function(game){
-        game.world.setBounds(game.world.bounds.centerX, game.world.bounds.centerY, 1920, 1200);
         game.stage.backgroundColor = "#0B65F8";
         
-        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');
-        
+        this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'ship');  
+        this.player.anchor.set(0.5);
+        console.log(this.player)
+        game.physics.arcade.enable(this.player);
     },
     update: function(game) {
-        
+
     },
-    render: function(game) {}
+    render: function(game) {
+        game.debug.geom(new Phaser.Point(this.player.pivot.x, this.player.pivot.y), '#ffff00');
+    }
 };
